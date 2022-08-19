f258fbe1049f5eaad4f4d7e268283b5f31aded32
rambtech111 <rambtech111@gmail.com>
2022-08-19 23:12:59 +0530

    more power

--- src/component/pages/Attributes.js
+++ src/component/pages/Attributes.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Attributes() {
     return (
         <div>
-               al dal asd lal k  this is tesing noew page kl dka  asd as  new chwnage here  
+               more power code al dal asd lal k  this is tesing noew page kl dka  asd as  new chwnage here  
         </div>
     )
 }
