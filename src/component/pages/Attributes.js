43f7fa2c36a641a6a28648ca3cfe880e359e2417
rambtech111 <rambtech111@gmail.com>
2022-08-20 16:02:06 +0530

    meow ps

--- src/component/pages/Attributes.js
+++ src/component/pages/Attributes.js
@@ -3,11 +3,9 @@ import React from 'react'
 function Attributes() {
     return (
         <div>
-<<<<<<< HEAD
+
                 this is Attribute page
-=======
-               al dal asd lal k  this is Attribute page kl dka  asd as  new chwnage here   i like this gowofmokmokm
->>>>>>> 34166ea8d902e0cb61954937ade2758cddfa8eca
+
         </div>
     )
 }
