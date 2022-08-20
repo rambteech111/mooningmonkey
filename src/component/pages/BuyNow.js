53c61ec3f2cabefbace1bd42a0df2b4a8e29b993
ram singh <66739301+rambteech111@users.noreply.github.com>
2022-08-20 16:05:53 +0530

    Update BuyNow.js

--- src/component/pages/BuyNow.js
+++ src/component/pages/BuyNow.js
@@ -3,7 +3,7 @@ import React from 'react'
 function BuyNow() {
     return (
         <div>
-            this is Buy Now Page is hosing sl
+            this is Buy Now Page is hosing sl more power to nwtm wml wekmwe lkmewlk rwe
         </div>
     )
 }
