0960a6f951d5d53a065b3486f757e4ddaa43d90f
rambtech111 <rambtech111@gmail.com>
2022-08-20 13:57:35 +0530

    sad  asD

--- src/component/pages/Attributes.js
+++ src/component/pages/Attributes.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Attributes() {
     return (
         <div>
-               more power code al dal asd lal k  this is tesing noew page kl dka  asd as  new chwnage here  
+                this is Attribute page
         </div>
     )
 }
